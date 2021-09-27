0377fa3c5351a34eac78b02f45997b03293a95f2
Hebert <hebertphp@gmail.com>
2021-09-27 11:38:12 -0300

    PBE_2a 27/09

---
+++ aula5/par_impar.php
@@ -0,0 +1,13 @@
+<?php
+$num=isset($_GET["num"])?$_GET["num"]:0;
+echo "Teste, num = $num";//par_impar.php?num=28
+/*
+Elaborar um código para verificar se um número inteiro
+é par ou ímpar, o sódigo deverá mostrar o número e na
+frente do número, PAR ou ímpar.
+Exemplo: 28 - PAR
+         275 - ÍMPAR
+ 5  % 2 = 1 - ÍMPAR
+ 18 % 2 = 0 - PAR         
+*/
+?>
