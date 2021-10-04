f40347857fcd8957db89a4d011244011e186aea8
Hebert <hebertphp@gmail.com>
2021-10-04 11:43:04 -0300

    PBE_2a 04/10

--- aula5/aula5_091.php
+++ aula5/aula5_091.php
@@ -3,7 +3,7 @@ $i=7; // inicialização do nosso contador;
 do {
   echo $i . "<br> ";
   $i+=2; // incremento do contador
-} while ($i < 5);
+} while ($i <= 5);
 echo "Ultimo valor para variável i = $i"; 
 /*
 Saída:
