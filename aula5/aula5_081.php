2714ce75484c5e2e1936116872c9429a1d9b0623
Hebert <hebertphp@gmail.com>
2021-10-04 10:38:14 -0300

    PBE_2a 04/10

--- aula5/aula5_081.php
+++ aula5/aula5_081.php
@@ -4,14 +4,24 @@ while ($i <= 5) {
   echo $i . "<br> ";
   $i++;
 }
+echo "<h1>Desafio - 1</h1>";
+$i=0; // inicialização do contador;
+while ($i <= 5) {
+  echo $i . "<br> ";
+  $i++;
+}
 /*
 Perguntas:
-1) Qual o valor inicial?
-2) Qual a condição?
-3) Qual o contador?
-4) Qual o último valor impresso?
-5) Qual o valor que torna a condição como falsa?
+1) Qual o valor inicial? 0
+2) Qual a condição? $i<=5
+3) Qual o contador? $i++
+4) Qual o último valor impresso? 5
+5) Qual o valor que torna a condição como falsa? 6
 Desafio - 1
+Saída:
+7
+8
+9
 
 Desafio - 2
 
