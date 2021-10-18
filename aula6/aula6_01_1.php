ab90c30790e7d3ed5f09178b81a936426253c2e4
Hebert <hebertphp@gmail.com>
2021-10-18 10:02:39 -0300

    PBE_2a 18/10

--- aula6/aula6_01_1.php
+++ aula6/aula6_01_1.php
@@ -4,8 +4,14 @@ $a[1]=20;
 $a[2]=158;
 
 $nome=array("Astrogildo","Belarmina","Pafuncia","Abelardo");
-//          
+//              0            1           2          3
 
-echo $nome[3];
+$fruta[]="laranja";
+$fruta[]="jaca";
+$fruta[]="limão";
+$fruta[]="maracujá";
+$fruta[]="caju";
+
+echo $fruta[3];
 
 ?>
