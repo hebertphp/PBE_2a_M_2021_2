336d4c55079986d2f1009e35e25290ac6f875fc6
Hebert <hebertphp@gmail.com>
2021-10-25 09:57:34 -0300

    PBE_2a 25/10/21

--- aula6/aula6_09_1.php
+++ aula6/aula6_09_1.php
@@ -5,8 +5,20 @@ $usuario = array(
  "email" => "teste@teste.com"
 );
 foreach ($usuario as $pos => $valor) {
- echo $pos . " - " . $valor . "<br/>";
+//  echo $pos . " - " . $valor . "<br/>";
+// echo $valor." ";
+echo $pos." ";
+}
+
+echo "<hr>";
+
+$uf = array("SP","RJ","MG","ES","RS","SC","PB","PR","BA","DF","SE");
+foreach ($uf as $pos =>$valor){
+ echo $pos . " ";
 }
 
 
+
+// comentário em PHP nos editores de código atalho--> [Ctrl][;] Ctrl + ;
 ?>
+<!-- comentário em HTML -->
