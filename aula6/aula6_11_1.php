9e942be64853253fe1cb5466de9966a08fd0cd45
Hebert <hebertphp@gmail.com>
2021-10-25 10:42:24 -0300

    PBE_2a 25/10/21

--- aula6/aula6_11_1.php
+++ aula6/aula6_11_1.php
@@ -1,8 +1,8 @@
 <?php
-$nomes=array("Fulano","Beltrano","Sicrano","Beltrano");
-echo count($nomes)."<br>";//
+$nomes=array("Fulano","Beltrano","Sicrano","Beltrano","Sicrano");
+echo count($nomes)."<br>";//5
 $resultado=array_unique($nomes);
-echo count($nomes)."<br>";//
+echo count($resultado)."<br>";//3
 foreach($resultado as $pos=>$valor){
  echo "Nomes[$pos]: " . $valor;
  echo "<br>";
