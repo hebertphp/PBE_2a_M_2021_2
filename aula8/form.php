ed5052b18ff8f53a03a9c52f1fa56297c63aa080
Hebert <hebertphp@gmail.com>
2021-11-22 09:44:29 -0300

    PBE_2a 22/11/21

---
+++ aula8/form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Form</title>
+</head>
+<body>
+    <form action="recebe.php">
+        Nome <input type="text" name="nome">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
