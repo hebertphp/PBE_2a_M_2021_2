87830c3ffe569be44bf2239898c7e897d80e1f50
Hebert <hebertphp@gmail.com>
2021-11-22 10:24:54 -0300

    PBE_2a 22/11/21

--- aula8/form.php
+++ aula8/form.php
@@ -6,12 +6,23 @@
 </head>
 <body>
     <pre>
-    <form action="recebe.php" method="post">
-        Nome  <input type="text" name="nome">
+    <form action="recebe.php" method="post" autocomplete="off">
+        Nome  <input type="text" name="nome" required autofocus>
 
-        Senha <input type="password" name="senha">
+        Senha <input type="password" name="senha" required>
+              <input type="hidden" name="oculto" value="Um valor">  
 
-              <input type="submit">
+        RA    <input type="number" name="ra" required>      
+
+        Selecione o sistema operacional
+
+        Windows <input type="checkbox" name="windows" value="windows">
+        Linux   <input type="checkbox" name="linux" value="linux">
+        MacOS   <input type="checkbox" name="macos" value="macos">
+        Android <input type="checkbox" name="android" value="android">
+
+
+                <input type="submit"> <input type="reset">
     </form>
     </pre>
 </body>
