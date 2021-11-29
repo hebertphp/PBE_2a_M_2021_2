19e9d6bed8a2f95ee56a9468b31ab25215bdcc8c
Hebert <hebertphp@gmail.com>
2021-11-29 10:07:04 -0300

    PBE_2a 29/11/21

--- aula9/aula9_02.php
+++ aula9/aula9_02.php
@@ -1,6 +1,13 @@
 <?php
 function boas_vindas() {
-    $quem=isset($GLOBALS["alguem"])?$GLOBALS["alguem"]:"você mesmo";
+    // $quem=isset($GLOBALS["alguem"])?$GLOBALS["alguem"]:"você mesmo";
+
+    if(isset($GLOBALS["alguem"])){
+        $quem=$GLOBALS["alguem"];
+    }
+    else{
+        $quem="você mesmo";
+    }
     echo "<h1>Seja bem vindo(a), $quem!</h1>";
 }
 // $alguem="Roberta";
