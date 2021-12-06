2984ec24f15422efcb2c50dc4fdfbe46ea16d948
Hebert <hebertphp@gmail.com>
2021-12-06 10:57:04 -0300

    PBE_2a 06/12/21

---
+++ aula9/duvida1.php
@@ -0,0 +1,6 @@
+<?php
+
+$a=125;
+$b="oi";
+echo "<pre>";print_r($GLOBALS);echo "<pre>";
+?>
---
+++ aula9/duvida2.php
@@ -0,0 +1,9 @@
+<?php
+$arr=array(
+    "nome"=>"Jose",
+    "nota"=>7.5
+);
+foreach($arr as $a=>$b){
+    echo $b." ";
+}
+?>
